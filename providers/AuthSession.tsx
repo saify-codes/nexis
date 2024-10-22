c2890712689c319934d9e0aadcc9b8dd77c1c4af
saify <sarfaraz.symits@gmail.com>
2024-10-22 22:19:20 +0500

    cleaning

--- providers/AuthSession.tsx
+++ providers/AuthSession.tsx
@@ -29,7 +29,7 @@ export default function ({ children }: { children: React.ReactNode }) {
 
                     }
                 }
-
+                
                 dispatch(destroySession())
             })
     }, [])
